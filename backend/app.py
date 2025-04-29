2b1bd2b4fdb9d7b7cafa3318768eeae164d6eaf4
ryzac04 <rzc044@gmail.com>
2025-04-29 05:23:24 -0500

    app.py setup; models.py Friends class; db.py renamed to routes.py;

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,14 @@
-from db import db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
-print("This is my app.py file.")
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app) 
+
+if __name__ == "__main__":
+    app.run(debug=True)
